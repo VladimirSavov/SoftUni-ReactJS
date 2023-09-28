ecd296abe9a5f37c342b07432aeda52617e3c074
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2023-09-28 23:58:10 +0300

    Add files via upload

---
+++ 01.first-react-app/src/components/Main.js
@@ -0,0 +1,11 @@
+function Main() {
+    return (
+        <main className="site-main">
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae commodi ipsam in placeat aspernatur et excepturi quasi id eligendi repellendus.
+            </p>
+        </main>
+    );
+};
+
+export default Main;
